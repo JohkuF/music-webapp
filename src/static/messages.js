68998b75c49c83f4aa0fb9b7d19371bcb5ee5486
Johannes <joh@nnes.fi>
2024-05-16 15:11:12 +0300

    format

--- src/static/messages.js
+++ src/static/messages.js
@@ -1,35 +1,35 @@
 function sanitize(string) {
-    const map = {
-      "&": "&amp;",
-      "<": "&lt;",
-      ">": "&gt;",
-      '"': "&quot;",
-      "'": "&#x27;",
-      "/": "&#x2F;",
-    };
-    const reg = /[&<>"'/]/gi;
-    return string.replace(reg, (match) => map[match]);
-  }
+  const map = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#x27;",
+    "/": "&#x2F;",
+  };
+  const reg = /[&<>"'/]/gi;
+  return string.replace(reg, (match) => map[match]);
+}
 
-  // Fetch messages
-  async function fetchMessages() {
-    var id = "{{ song_id }}";
+// Fetch messages
+async function fetchMessages() {
+  var id = "{{ song_id }}";
 
-    try {
-      const response = await fetch(`/messages/${id}`);
-      const messages = await response.json();
+  try {
+    const response = await fetch(`/messages/${id}`);
+    const messages = await response.json();
 
-      var id_extra = 0;
-      // Add messages to the containers
-      const messagesContainer = document.getElementById(
-        `messagesContainer-${id}`
-      );
+    var id_extra = 0;
+    // Add messages to the containers
+    const messagesContainer = document.getElementById(
+      `messagesContainer-${id}`
+    );
 
-      messages.reverse().forEach((message) => {
-        username = sanitize(message.username);
-        content = sanitize(message.content);
+    messages.reverse().forEach((message) => {
+      username = sanitize(message.username);
+      content = sanitize(message.content);
 
-        const messageCard = `
+      const messageCard = `
           <div class="card mb-4" id="messages-${id}-${id_extra}">
             <div class="card-body">
               <p>${content}</p>
@@ -40,10 +40,10 @@ function sanitize(string) {
               </div>
             </div>
           </div>`;
-        id_extra++;
-        messagesContainer.insertAdjacentHTML("beforeend", messageCard);
-      });
-    } catch (error) {
-      console.error("Error fetching messages: ", error);
-    }
+      id_extra++;
+      messagesContainer.insertAdjacentHTML("beforeend", messageCard);
+    });
+  } catch (error) {
+    console.error("Error fetching messages: ", error);
   }
+}
