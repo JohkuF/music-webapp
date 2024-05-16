78312946a6f98c8dfdf8980b158689113ac3f3dc
Johannes <joh@nnes.fi>
2024-05-16 15:08:02 +0300

    vote button animation

--- src/static/audio.js
+++ src/static/audio.js
@@ -1,55 +1,55 @@
 function toggleIcons(icon) {
-    const listPlayIcons = document.querySelectorAll(".icon-play");
-    const listPauseIcons = document.querySelectorAll(".icon-pause");
-    var clickedIconPlay = icon.parentElement.querySelector(".icon-play");
-    var clickedIconPause = icon.parentElement.querySelector(".icon-pause");
+  const listPlayIcons = document.querySelectorAll(".icon-play");
+  const listPauseIcons = document.querySelectorAll(".icon-pause");
+  var clickedIconPlay = icon.parentElement.querySelector(".icon-play");
+  var clickedIconPause = icon.parentElement.querySelector(".icon-pause");
 
-    // switch previous off
-    const playIconsArray = Array.from(listPlayIcons); // Convert NodeList to array
+  // switch previous off
+  const playIconsArray = Array.from(listPlayIcons); // Convert NodeList to array
 
-    playIconsArray.forEach((item, index) => {
-      if (
-        item.classList.contains("d-none") &&
-        parseInt(item.id.split("-")[1]) !== parseInt(icon.id.split("-")[1])
-      ) {
-        item.classList.toggle("d-none");
-        listPauseIcons[index].classList.toggle("d-none");
-      }
-    });
+  playIconsArray.forEach((item, index) => {
+    if (
+      item.classList.contains("d-none") &&
+      parseInt(item.id.split("-")[1]) !== parseInt(icon.id.split("-")[1])
+    ) {
+      item.classList.toggle("d-none");
+      listPauseIcons[index].classList.toggle("d-none");
+    }
+  });
 
-    // Switch clicked on
-    clickedIconPlay.classList.toggle("d-none");
-    clickedIconPause.classList.toggle("d-none");
+  // Switch clicked on
+  clickedIconPlay.classList.toggle("d-none");
+  clickedIconPause.classList.toggle("d-none");
 
-    songName = clickedIconPlay.parentElement.parentElement
-      .querySelector("div#songNameContainer")
-      .querySelector("p").textContent;
+  songName = clickedIconPlay.parentElement.parentElement
+    .querySelector("div#songNameContainer")
+    .querySelector("p").textContent;
 
-    // Current song id
-    currentSongId = parseInt(clickedIconPlay.id.split("-")[1]);
+  // Current song id
+  currentSongId = parseInt(clickedIconPlay.id.split("-")[1]);
 
-    getAudioSong(currentSongId, songName);
+  getAudioSong(currentSongId, songName);
 
-    // Toggle offcanvas on if not already 
-    // (The popup with the music player)
-    getOffCanvas();
-  }
+  // Toggle offcanvas on if not already
+  // (The popup with the music player)
+  getOffCanvas();
+}
 
-  function getOffCanvas() {
-    // Get reference to the offcanvas
-    const offcanvas = new bootstrap.Offcanvas(
-      document.getElementById("offcanvasScrolling")
-    );
+function getOffCanvas() {
+  // Get reference to the offcanvas
+  const offcanvas = new bootstrap.Offcanvas(
+    document.getElementById("offcanvasScrolling")
+  );
 
-    // Toggle the offcanvas
-    offcanvas.toggle();
-  }
+  // Toggle the offcanvas
+  offcanvas.toggle();
+}
 
-  function getAudioSong(songId, songName) {
-    songContainer = document.getElementById("musicContainer");
-    songLabel = document.getElementById("offcanvasBottomLabel");
+function getAudioSong(songId, songName) {
+  songContainer = document.getElementById("musicContainer");
+  songLabel = document.getElementById("offcanvasBottomLabel");
 
-    const songCard = `
+  const songCard = `
       <div id="musicContainer">
         <audio controls autoplay style="width: 100%" id="audio">
           <source src="stream/${songId}" type="audio/mp3" />
@@ -57,6 +57,6 @@ function toggleIcons(icon) {
       </div>
     `;
 
-    songContainer.innerHTML = songCard;
-    songLabel.textContent = songName;
-  }
+  songContainer.innerHTML = songCard;
+  songLabel.textContent = songName;
+}
--- src/static/vote.js
+++ src/static/vote.js
@@ -1,42 +1,98 @@
 function toggleVote(icon) {
-    var otherId = icon.id
-    if (otherId.split("-")[1] === "on") {
-        otherId = otherId.replace("on", "off");
-    } else {
-        otherId = otherId.replace("off", "on");
+  var change = toggle(icon);
+
+  // Turn off the other vote
+  // Check if the other vote is on -> turn it off
+  if (checkOthervote(icon)) {
+    const otherVoteON = getOtherVoteON(icon);
+    const otherVoteOFF = getOtherVoteOFF(icon);
+
+    // Check which one has the "d-none"
+    if (
+      otherVoteOFF.getAttribute("class").includes("d-none") &&
+      change === "on"
+    ) {
+      toggle(otherVoteON);
     }
+  }
+}
 
-    var other = icon.parentElement.querySelector("#"+otherId);
-    var clickedClass = icon.getAttribute("class");
-    var otherClass = other.getAttribute("class");
-    
-    clickedClass += " d-none"
-    other.classList.remove("d-none")
-    icon.setAttribute("class", clickedClass.trim());
+function toggle(icon) {
+  var otherId = icon.id;
+  var change = otherId.split("-")[1];
 
+  if (change === "on") {
+    otherId = otherId.replace("on", "off");
+    change = "off";
+  } else {
+    otherId = otherId.replace("off", "on");
+    change = "on";
+  }
 
-    // Check if 
+  var other = icon.parentElement.querySelector("#" + otherId);
 
-    /*
-    // Update otherId based on the state of the clicked icon
-    
-    
-    if (clickedClass.includes("d-none")) {
-        // If "d-none" is present, remove it
-        clickedClass = clickedClass.replace("d-none", "");
-        
-    } else {
-        // If "d-none" is not present, add it
-        clickedClass += " d-none";
-    }
-    
-    if (otherClass.includes("d-none")) {
-        // If "d-none" is present, remove it
-        otherClass = otherClass.replace("d-none", "");
-    } else {
-        // If "d-none" is not present, add it
-        otherClass += " d-none";
-    }
-    
+  var clickedClass = icon.getAttribute("class");
+  //var otherClass = other.getAttribute("class");
+
+  clickedClass += " d-none";
+  other.classList.remove("d-none");
+
+  // Set change
+  icon.setAttribute("class", clickedClass.trim());
+
+  return change;
+}
+
+function checkOthervote(icon) {
+  /*
+    Check if other vote is on or off
+    on -> True
+    off -> False
     */
+
+  var otherVote = getOtherVoteON(icon);
+
+  // Get other vote
+
+  // Check if the "on" has d-none
+  if (otherVote.getAttribute("class").includes("d-none")) {
+    return false;
+  }
+  return true;
+}
+
+function getOtherVoteON(icon) {
+  /*
+  The other vote (upvote, downvote) ON-version
+  */
+  var otherVoteId = getOtherVoteId(icon);
+  otherVoteId = otherVoteId.replace("off", "on");
+
+  var otherVote = icon.parentElement.parentElement.parentElement.querySelector(
+    "#" + otherVoteId
+  );
+  return otherVote;
+}
+
+function getOtherVoteOFF(icon) {
+  /*
+    The other vote (upvote, downvote) ON-version
+  */
+  var otherVoteId = getOtherVoteId(icon);
+  otherVoteId = otherVoteId.replace("on", "off");
+  var otherVote = icon.parentElement.parentElement.parentElement.querySelector(
+    "#" + otherVoteId
+  );
+  return otherVote;
+}
+
+function getOtherVoteId(icon) {
+  var otherVoteId = icon.id;
+  if (otherVoteId.startsWith("upvote")) {
+    otherVoteId = otherVoteId.replace("upvote", "downvote");
+  } else {
+    otherVoteId = otherVoteId.replace("downvote", "upvote");
+  }
+
+  return otherVoteId;
 }
